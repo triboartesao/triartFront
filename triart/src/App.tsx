f8d8b2782b0362580c11395b0ea7ce499dd13280
unknown <ankiflavio@gmail.com>
2022-10-03 12:56:13 -0300

    Navbar implementada

--- triart/src/App.tsx
+++ triart/src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import Navbar from './components/statics/navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
+import Home from './paginas/home/Home';
+import Sobre from './paginas/sobre/Sobre';
+import Login from './paginas/login/Login';
 import './App.css';
+
+
 
 function App() {
   return (
-<>
-<Navbar/>
-</>
+    <BrowserRouter>
+      
+        <Navbar />
+
+        <div style={{minHeight: '100vh'}}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+
+          </Routes>
+        </div>
+        
+        <Footer />
+      
+    </BrowserRouter>
   );
 }
 
