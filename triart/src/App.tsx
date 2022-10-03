92c3f187c16dadf08ba593ba448247e0343fa16c
Mirelle Mota <mirelle.motta06@gmail.com>
2022-10-03 11:38:12 -0300

    Footer Mirelle

--- triart/src/App.tsx
+++ triart/src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
+import Home from './paginas/home/Home';
+import Sobre from './paginas/sobre/Sobre';
+import Login from './paginas/login/Login';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <BrowserRouter>
       
-      </header>
-    </div>
+        <Navbar />
+
+        <div style={{minHeight: '100vh'}}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+
+          </Routes>
+        </div>
+        
+        <Footer />
+      
+    </BrowserRouter>
   );
 }
 
