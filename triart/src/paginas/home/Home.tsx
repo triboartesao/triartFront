042fb4b72d3cc4ae1ca8afcc763973b61c354415
Sandy Martins <sandycris.luar@gmail.com>
2022-10-16 01:04:12 -0300

    adicionando efeito hover no logo

--- triart/src/paginas/home/Home.tsx
+++ triart/src/paginas/home/Home.tsx
@@ -2,22 +2,31 @@ import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 
-
-
-
 function Home() {
   return (
     <section>
       <div>
-        <h2 className="fraseHome">Conectando o empreendedor rural com o mundo!</h2>
-       <Link to="/produtos">
-        <button className='borderDemo'>Vá as compras!</button>
+        <h2 className="fraseHome">
+          Conectando o empreendedor rural com o mundo!
+        </h2>
+        <Link to="/produtos">
+          <button className="borderDemo">Vá as compras!</button>
+        </Link>
+        <Link to="/produtos">
+          <img
+            src="https://i.imgur.com/8Q3oCTF.png"
+            alt="Logo"
+            className="bgLogo"
+          />
         </Link>
       </div>
       <div>
-        <img src="https://i.imgur.com/djroexx.jpg" alt="Imagem de Fundo" className="bgHome" />
+        <img
+          src="https://i.imgur.com/nyLJUod.jpg"
+          alt="Imagem de Fundo"
+          className="bgHome"
+        />
       </div>
-
     </section>
   );
 }
