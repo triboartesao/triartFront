e0fa86e6fe53ea65285a668b2e2d4c3e59b50f74
Brenda Chagas <brenda.chsilva@hotmail.com>
2022-10-17 19:43:00 -0300

    Estilazando Footer

--- triart/src/components/statics/footer/Footer.tsx
+++ triart/src/components/statics/footer/Footer.tsx
@@ -1,46 +1,100 @@
-import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {Typography, Grid } from '@material-ui/core';
-import './Footer.css';
-import { Box } from '@mui/material';
+import React from "react";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { Typography, Box, Grid } from "@mui/material";
+import { useSelector } from "react-redux";
+import { TokenState } from '../../../store/tokens/TokensReducer';
+import "./Footer.css"; 
 
 function Footer() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box className='box1'>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                <FacebookIcon className='redes' />
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                <InstagramIcon className='redes' />
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <LinkedInIcon className='redes' />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box className='box2'>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2022 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
-    )
-}
 
-export default Footer;
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+      (state) => state.tokens
+    );
+  
+    var footerComponent;
+  
+    if(token != "") {
+      footerComponent = <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      
+      
+    >
+      <Grid alignItems="center" item xs={12}>
+        <Box className="box1">
+          <Box 
+            paddingTop={1}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography 
+              variant="h5"
+              align="center"
+              gutterBottom
+              className="textos"
+              
+              
+            
+            >
+              SIGA-NOS NAS REDES SOCIAIS!{" "}
+            </Typography>
+          </Box>
+          <Box className="iconsRedes">
+            <a
+              href="https://github.com/https-github-com-triboartesao"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHubIcon className="redes github"/>
+            </a>
+            <a
+              href="https://www.instagram.com/triart.gen/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <InstagramIcon className="redes instagram"  />
+            </a>
+          </Box>
+        </Box>
+        <Box className="box2">
+          <Box>
+            <Typography 
+              variant="subtitle2"
+              align="center"
+              gutterBottom
+              className="cr"
+           
+            >
+              © 2022 Copyright:
+            </Typography>
+          </Box>
+          <Box>
+              <Typography
+                variant="subtitle2"
+                gutterBottom
+                align="center"
+                className="cr"
+                
+              >
+                Generation Brasil
+              </Typography>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+    }
+  
+    return (
+      <>
+       {footerComponent} 
+      </>
+    );
+  }
+  
+  export default Footer;
+  
