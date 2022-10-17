c7400fe4483127c3a49e9730689b05998100d7fe
Matues <mateus.q.d@gmail.com>
2022-10-17 15:49:05 -0300

    Atualização Login/Cadastrar

--- triart/src/paginas/login/Login.tsx
+++ triart/src/paginas/login/Login.tsx
@@ -1,4 +1,3 @@
-
 import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Typography, TextField, Button } from '@material-ui/core'
 import { Grid, Box } from '@mui/material';
@@ -15,7 +14,8 @@ import { login } from '../../services/Service';
 import UserLogin from '../../models/UserLogin'
 import { useDispatch } from 'react-redux';
 import { addToken } from '../../store/tokens/Actions';
-
+import { toast } from 'react-toastify';
+import './Login.css'
 
 interface State {
   password: string;
@@ -36,111 +36,134 @@ function Login() {
     token: ''
   });
 
-  function updateModel(event: ChangeEvent<HTMLInputElement>){
+  function updateModel(event: ChangeEvent<HTMLInputElement>) {
     setUserLogin({
       ...userLogin,
       [event.target.name]: event.target.value
     })
   }
 
-  async function conectar(event: ChangeEvent<HTMLFormElement>){
+  async function conectar(event: ChangeEvent<HTMLFormElement>) {
     event.preventDefault();
-    try{
+    try {
       await login('usuarios/logar', userLogin, setToken)
 
-      alert('Usuário Logado com Sucesso!!!');
-    }catch(error){
-      alert('Erro ao Logar!!! Verifique os dados e tente novamente.')
+      toast.success('Usuário Logado com Sucesso!', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+      });
+    } catch (error) {
+      toast.error('Erro ao Logar!!! Verifique os dados e tente novamente.', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+      });
     }
   }
 
+
+
   useEffect(() => {
-    if(token !== ''){
+    if (token !== '') {
       dispatch(addToken(token))
       navigate('/home')
     }
   }, [token])
 
   const [values, setValues] = React.useState<State>({
-      password: '',
-      showPassword: false,
+    password: '',
+    showPassword: false,
   });
 
   const handleChange =
-  (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [prop]: event.target.value });
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
   };
 
-const handleClickShowPassword = () => {
-  setValues({
-    ...values,
-    showPassword: !values.showPassword,
-  });
-};
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
 
-const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-  event.preventDefault();
-};
+ 
+ 
 
 
   return (
 
-    <Grid container direction='row' justifyContent='center' alignItems='center'>
+    <Grid container direction='row' justifyContent='center' alignItems='center' className='fundo'>
       <Grid alignItems='center' xs={6} justifyContent='center'>
-        <Box>
-          <form onSubmit={conectar}>
-            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}>Login</Typography>
+        <Box paddingX={6} >
+          <form onSubmit={conectar}  className='fundo2'>
+            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }} className="escolha">Login</Typography>
             <TextField
+              onChange={(event: ChangeEvent<HTMLInputElement>) => updateModel(event)}
+              value={userLogin.usuario}
+              id="usuario"
+              name="usuario"
+              label="Usuário"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <FormControl fullWidth margin='normal' variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password" className="escolha">Senha</InputLabel>
+              <OutlinedInput
+                id="senha"
+                name='senha'
+                type={values.showPassword ? 'text' : 'password'}
+                value={userLogin.senha}
                 onChange={(event: ChangeEvent<HTMLInputElement>) => updateModel(event)}
-                value={userLogin.usuario}
-                id="usuario"
-                name="usuario"
-                label="Usuário"
-                variant="outlined"
-                fullWidth
-                margin="normal"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
               />
-              <FormControl fullWidth margin='normal' variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
-                <OutlinedInput
-                  id="senha"
-                  name='senha'
-                  type={values.showPassword ? 'text' : 'password'}
-                  value={userLogin.senha}
-                  onChange={(event: ChangeEvent<HTMLInputElement>) => updateModel(event)}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Password"
-                />
-              </FormControl>
-              <Box marginTop={2}>
-                <Button type='submit' variant='contained' color='primary'>Entrar</Button>
-              </Box>
+            </FormControl>
+            <Box marginTop={2}>
+              <Button type='submit' variant="outlined" className='btn'>Entrar</Button>
+            </Box>
           </form>
           <Box display='flex' justifyContent='center' marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não possui conta?</Typography>
-                        </Box>
-          
-            <Link to= '/cadastrar'>
-               <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
-             </Link>
-                       
+            <Box marginRight={1}>
+              <Typography variant='subtitle1' gutterBottom align='center'>Não possui conta?</Typography>
+            </Box>
+
+            <Link to='/cadastrar'>
+              <Typography variant='subtitle1' gutterBottom align='center' className='textos1 cadastro'>Cadastre-se</Typography>
+            </Link>
+
           </Box>
-        </Box>
-
-
+        </Box>   
       </Grid>
+      <Grid xs={6} className='imagemLogo'>
+        </Grid >
     </Grid>
 
 
