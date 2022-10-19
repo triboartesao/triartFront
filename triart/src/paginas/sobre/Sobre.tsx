9774d6ba8b357e8430e5e5367c92b77d294cde50
Sandy Martins <sandycris.luar@gmail.com>
2022-10-19 14:59:34 -0300

    Página sobre com cards sobre eventos

--- triart/src/paginas/sobre/Sobre.tsx
+++ triart/src/paginas/sobre/Sobre.tsx
@@ -51,6 +51,7 @@ function Sobre() {
   <h4 className='h6textPlace'>Acesse as informações de galerias de arte contemporânea, museus e fundações de arte</h4>
   </div>
 
+
   <main className='carousel-main'> 
 <div className="container">
   <div className="carousel">
@@ -63,16 +64,56 @@ function Sobre() {
     <div className="carousel__face"><span> <a className='linkCarousel' href="https://www.farolsantander.com.br/#/">Farol Santander</a></span></div>
     <div className="carousel__face"><span> <a className='linkCarousel' href="https://www.archdaily.com.br/br/786322/porto-seguro-cultural-center-sao-paulo-arquitetura">Espaço Porto Seguro</a></span></div>
     <div className="carousel__face"><span> <a className='linkCarousel' href="https://www.prefeitura.sp.gov.br/cidade/secretarias/cultura/bma/">Biblioteca Mário de Andrade</a></span></div>
-
   </div>
 </div>
 </main>
 
-  </section>
 
+{/* ADICIONANDO CARDS DE CURSOS */}
 
-
-
+<section className='cardCursos'>
+<div className="band">
+<div className="item-2">
+          <a href="https://webdesign.tutsplus.com/articles/how-to-conduct-remote-usability-testing--cms-27045" className="card">
+            <div className="thumb" style={{backgroundImage: `url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/users-2.png")` }}></div>
+            <article className='articleCard'>
+              <h1 className='h1Card'>How to Conduct Remote Usability Testing</h1>
+              <span>Harry Brignull</span>
+            </article>
+          </a>
+    </div>
+    <div className="item-3">
+      <a href="https://design.tutsplus.com/articles/envato-tuts-community-challenge-created-by-you-july-edition--cms-26724" className="card">
+        <div className="thumb" style={{backgroundImage: `url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/users-2.png")` }}></div>
+        <article className='articleCard'>
+          <h1 className='h1Card'>Created by You, July Edition</h1>
+          <p className='h1Card'>Welcome to our monthly feature of fantastic tutorial results created by you, the Envato Tuts+ community! </p>
+          <span>Melody Nieves</span>
+        </article>
+      </a>
+    </div>
+    <div className="item-4">
+          <a href="https://webdesign.tutsplus.com/tutorials/how-to-code-a-scrolling-alien-lander-website--cms-26826" className="card">
+            <div className="thumb" style={{backgroundImage: `url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/users-2.png")` }}></div>
+            <article className='articleCard'>
+              <h1 className='h1Card'>How to Code a Scrolling “Alien Lander” Website</h1>
+              <p className='pCard'>Well be putting things together so that as you scroll down from the top of the page youll see an “Alien Lander” making its way to touch down.</p>
+              <span>Kezz Bracey</span>
+            </article>
+          </a>
+    </div>
+    <div className="item-5">
+          <a href="https://design.tutsplus.com/tutorials/stranger-things-inspired-text-effect--cms-27139" className="card">
+            <div className="thumb" style={{backgroundImage: `url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/users-2.png")` }}></div>
+            <article className='articleCard'>
+              <h1 className='h1Card'>How to Create a “Stranger Things” Text Effect in Adobe Photoshop</h1>
+              <span>Rose</span>
+            </article>
+          </a>
+    </div>
+  </div> 
+</section>
+</section>
 </body>
 
  
